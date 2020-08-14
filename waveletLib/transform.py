f76e84e11622812106d65b464258234577f7bc30
Aleksander Stando <alek.stando@gmail.com>
2020-08-14 20:13:42 +0200

    Zmieniono algorytm liczenia falki, teraz od razu liczone są wszystkie poziomy

--- waveletLib/transform.py
+++ waveletLib/transform.py
@@ -1,5 +1,23 @@
+from util import *
+
 class Transform:
     def __init__(self, wavelet):
         self.wavelet = wavelet
     def decompose(self, signal, level):
-        return self.wavelet.decompose(signal, level)
+        coeffs_list = []
+        a = signal
+        for i in range(level):
+            a, d = self.dwt(a)
+            coeffs_list.append(d)
+
+        coeffs_list.append(a)
+        coeffs_list.reverse()
+
+        return coeffs_list
+    def dwt(self, data):
+        output_len = len(data) // 2 + len(data) % 2
+
+        cA = downsampling_convolution(self, data, self.wavelet.dec_lo, self.wavelet.step, "SYMMETRIC")
+        cD = downsampling_convolution(self, data, self.wavelet.dec_hi, self.wavelet.step, "SYMMETRIC")
+
+        return cA, cD
--- waveletLib/wavelet.py
+++ waveletLib/wavelet.py
@@ -1,29 +1,9 @@
 class Wavelet:
     def __init__(self):
-        self._name = None;
-        self._motherWavelength = 0;
-        self._transformWavelength = 0;
-        self._scalingDeCom = None;
-        self._waveletDeCom = None;
-    # length in NOT ALWAYS len(signal) since we might be working to part of the array
-    def decompose(self, signal, length):
-        arrHilb = [0] * length
-        h = length // 2
-        for i in range(0, h):
-            arrHilb[i] = 0
-            arrHilb[i + h] = 0
-            for j in range(0, self._motherWavelength):
-                k = i*2 + j
-                while k >= len(arrHilb):
-                    k -= len(arrHilb)
-
-                arrHilb[ i ] += signal[k] * self._scalingDeCom[ j ]
-                arrHilb[ i + h ] += signal[ k ] * self._waveletDeCom[ j ]
-        return arrHilb
-    def _buildOrthonormalSpace(self):
-        self._waveletDeCom = [0] * self._motherWavelength
-        for i in range(0, self._motherWavelength):
-            if i % 2 == 0:
-                self._waveletDeCom[i] = self._scalingDeCom[ (self._motherWavelength - 1) - i]
-            else:
-                self._waveletDeCom[i] = - self._scalingDeCom[ (self._motherWavelength - 1) - i]
+        self.dec_lo = []
+        self.dec_hi = []
+        self.dec_len = 0
+        self.family_name = None
+        self.short_name = None
+        self.support_width = 0
+        self.step = 2

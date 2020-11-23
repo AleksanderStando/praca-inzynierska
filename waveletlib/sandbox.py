174d79d55a0839225737588b3905d04e47f7ac80
Aleksander Stando <alek.stando@gmail.com>
2020-11-24 00:17:24 +0100

    Dodane unit testy w wavelet lib

---
+++ waveletlib/sandbox.py
@@ -0,0 +1,45 @@
+from transform import Transform
+import wavelet
+from Daubechies import Daubechies
+from Symlet import Symlet
+from Coiflet import Coiflet
+from draw_wavelet import *
+
+import pywt
+
+
+
+x = [1,2,3,4,5,6,7,8, 9] * 3
+
+print("waveletlib:")
+wave = Transform(Daubechies(2))
+print(wave.decompose(x, 3, "PERIODIC"))
+
+print("pywt library:")
+
+coeffs = pywt.wavedec(x, 'db2', level=3, mode='symmetric')
+print(coeffs)
+
+print("***")
+
+print("waveletlib:")
+wave = Transform(Symlet(2))
+print(wave.decompose(x, 2))
+
+print("pywt library:")
+
+coeffs = pywt.wavedec(x, 'sym2', level=2, mode='symmetric')
+print(coeffs)
+
+print("***")
+
+print("waveletlib:")
+wave = Transform(Coiflet(1))
+print(wave.decompose(x, 2))
+
+print("pywt library:")
+
+coeffs = pywt.wavedec(x, 'coif1', level=2, mode='symmetric')
+print(coeffs)
+
+draw_wavelet(Daubechies(3))
--- waveletlib/test.py
+++ waveletlib/test.py
@@ -1,43 +1,24 @@
 from transform import Transform
-import wavelet
 from Daubechies import Daubechies
-from Symlet import Symlet
-from Coiflet import Coiflet
-from draw_wavelet import *
+import unittest
+import os
+from scipy.io import wavfile
 
-import pywt
+class TestWavelet(unittest.TestCase):
+    def test_correct_size(self):
+        wave = Transform(Daubechies(1))
+        signal = [1] * 1024
+        coeffs = wave.decompose(signal, 10)
+        self.assertEqual(len(coeffs[0]), 1)
+        self.assertEqual(len(coeffs[1]), 1)
+        self.assertEqual(len(coeffs[2]), 2)
+        self.assertEqual(len(coeffs[10]), 512)
+    def test_incorrect_size(self):
+        wave = Transform(Daubechies(3))
+        signal = [1] * 1023
+        with self.assertRaises(Exception) as context:
+             wave.decompose(signal, 10)
 
-x = [1,2,3,4,5,6,7,8, 9] * 3
 
-print("waveletlib:")
-wave = Transform(Daubechies(2))
-print(wave.decompose(x, 3, "PERIODIC"))
-
-print("pywt library:")
-
-coeffs = pywt.wavedec(x, 'db2', level=3, mode='periodic')
-print(coeffs)
-
-print("***")
-
-print("waveletlib:")
-wave = Transform(Symlet(2))
-print(wave.decompose(x, 2))
-
-print("pywt library:")
-
-coeffs = pywt.wavedec(x, 'sym2', level=2, mode='symmetric')
-print(coeffs)
-
-print("***")
-
-print("waveletlib:")
-wave = Transform(Coiflet(1))
-print(wave.decompose(x, 2))
-
-print("pywt library:")
-
-coeffs = pywt.wavedec(x, 'coif1', level=2, mode='symmetric')
-print(coeffs)
-
-draw_wavelet(Daubechies(3))
+if __name__ == '__main__':
+    unittest.main()
